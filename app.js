5d14ceaa0e5ab8e560ebf122000cd5d8972f5982
brittney-joyce <brittney.magsino@gmail.com>
2018-06-07 23:27:41 -0700

    retry

--- app.js
+++ app.js
@@ -71,7 +71,15 @@ d3.csv("degreeData.csv", function(err, csvdata) {
      .attr("fill", "pink")
      .attr("opacity", ".5");
 
-
+     chartGroup.selectAll("text")
+            .attr("x", function(d){ return d.x; })
+            .attr("y", function(d){ return d.y + 5; })
+            .attr("text-anchor", "middle")
+            .text(function(d){ return d["state"]; })
+            .style({
+                "fill":"white",
+                "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
+                "font-size": "12px"
 
      // Step 6: Initialize tool tip
      // ==============================
