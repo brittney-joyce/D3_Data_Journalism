dbef4c6ea64484392f296cc3b622842d32179ce5
brittney-joyce <brittney.magsino@gmail.com>
2018-06-07 23:05:19 -0700

    revert

--- app.js
+++ app.js
@@ -98,14 +98,14 @@ d3.csv("degreeData.csv", function(err, csvdata) {
        // Create axes labels
        chartGroup.append("text")
          .attr("transform", "rotate(-90)")
-         .attr("y", 0 - margin.left )
+         .attr("y", 0 - margin.left + 40)
          .attr("x", 0 - (height / 2))
          .attr("dy", "1em")
          .attr("class", "axisText")
          .text("Income in Thousands $");
 
        chartGroup.append("text")
-         .attr("transform", `translate(${width / 2}, ${height + margin.top })`)
+         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
          .attr("class", "axisText")
          .text("Percentage of Population with a Bachelor's Degree");
      });
